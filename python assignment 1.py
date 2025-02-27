56c0cac757a037597f33f5fe58fc3454117bda5a
MuhammadNabeel228 <mn5987189@gmail.com>
2025-02-27 13:12:45 +0500

    Add files via upload

--- python assignment 1.py
+++ python assignment 1.py
@@ -1,52 +1,35 @@
-num = float(input("Enter a number:"))
-Enter number:2
->>> if num >0:
-	print ("Positive")
-elif num <0:
-	print ("Negative")
-else:
-	print ("Zero")
-
-	
-Positive
->>> year = int(input("Enter a year"))
-Enter a year2020
->>> if (year%400 == 0) or (year%4 == 0 and year%100!= 0):
-	print("Leap year")
-else:
-	print("Not a leap year")
-
-	
-Leap year
-marks = float(input("Enter student marks:"))
-if marks >= 85:
-    grade = 'A'
-elif marks >=70:
-    grade = 'B'
-elif marks >= 50:
-    grade = 'C'
-else:
-    grade = 'F'
-print (f"Grade:{grade}")
- num1 = float(input("Enter first number:"))
-Enter first number:2
->>> num2 = (input("Enter second number:"))
-Enter second number:5
->>> if op == '+':
-	result = num1 + num2
-elif op == '-':
-	result = num1 - num2
-elif op == '*':
-	result = num1 * num2
-elif op == '/':
-    if num2 == 0:
-        print("Error: Division by zero!")
-        exit()
-    result = num1 / num2
-else:
-    print("Invalid operator")
-    exit()
-
-print(f"Result:{result}")
-
-          
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #display several nunmbers
+>>> numbers = [10,20,30,40,50]
+>>> print("Displayed numbers:",numbers)
+Displayed numbers: [10, 20, 30, 40, 50]
+>>> #solve and show summation of 64+32
+>>> sum_result = 64+32
+>>> print("Summation of 64+32:",sum_result)
+Summation of 64+32: 96
+>>> #do the same as in 2, but make it sum = x+y
+>>> summation of 64+32
+SyntaxError: invalid syntax
+>>> #do the same as in 2, but make it sum = x+y
+>>> x = 64
+>>> y = 32
+>>> sum_xy = x+y
+>>> print("Summation of x+y:",sum_xy)
+Summation of x+y: 96
+>>> #print the word lucky inside s
+>>> s = "lucky"
+>>> print("The word inside s is:",s)
+The word inside s is: lucky
+>>> from datetime import date
+>>> today = date.today()
+>>> print (f"Today is {today.day}/{today.month}/{today.year}")
+Today is 26/2/2025
+>>> phone number = input("Enter your phone number")
+SyntaxError: invalid syntax
+>>> phone number = input("Enter your phone number")
+SyntaxError: invalid syntax
+>>> phone_number = input("Enter your phone number")
+Enter your phone number03000000000
+>>> print("your phone number is:",phone_number)
+your phone number is: 03000000000
